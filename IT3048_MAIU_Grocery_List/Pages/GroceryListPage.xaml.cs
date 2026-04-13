e7846123900c6e7c8f85a655f717959f381a25a5
Szalapbp <szalapbp@mail.uc.edu>
2026-04-13 18:09:24 -0400

    GroceryList, Household pages, Packages
    added the starters for grocerylist and household pages as well as added the package again

---
+++ IT3048_MAIU_Grocery_List/Pages/GroceryListPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace IT3048_MAIU_Grocery_List.Pages;
+
+public partial class GroceryListPage : ContentPage
+{
+	public GroceryListPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ IT3048_MAIU_Grocery_List/Pages/HouseholdPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace IT3048_MAIU_Grocery_List.Pages;
+
+public partial class HouseholdPage : ContentPage
+{
+	public HouseholdPage()
+	{
+		InitializeComponent();
+	}
+}
