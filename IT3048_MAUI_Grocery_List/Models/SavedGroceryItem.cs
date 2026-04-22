e1a33d18f92ef5282461517df5428c37623842e5
Ian <ianmkellenberger@gmail.com>
2026-04-22 13:20:06 -0400

    fixed saved list item display

--- IT3048_MAUI_Grocery_List/Models/SavedGroceryItem.cs
+++ IT3048_MAUI_Grocery_List/Models/SavedGroceryItem.cs
@@ -4,11 +4,12 @@ namespace IT3048_MAUI_Grocery_List.Models
 {
     public class SavedGroceryItem
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
         public int GroceryListId { get; set; }
         public string ItemName { get; set; }
         public decimal Price { get; set; }
         public bool IsChecked { get; set; }
     }
-
 }
--- IT3048_MAUI_Grocery_List/Pages/SavedListsPage.xaml.cs
+++ IT3048_MAUI_Grocery_List/Pages/SavedListsPage.xaml.cs
@@ -1,15 +1,13 @@
 using IT3048_MAUI_Grocery_List.ViewModels;
+using IT3048_MAUI_Grocery_List.Services;
 
 namespace IT3048_MAUI_Grocery_List.Pages;
 
 public partial class SavedListsPage : ContentPage
 {
-    public SavedListsPage()
+    public SavedListsPage(GroceryListRepository repo)
     {
         InitializeComponent();
-
-        BindingContext = new SavedListsViewModel(
-            App.DatabaseService.GroceryLists
-        );
+        BindingContext = new SavedListsViewModel(repo);
     }
 }
